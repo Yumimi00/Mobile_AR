4333f72537dc37df88ced46facf388caa7e2e7c5
Yumimi00 <nekoseeker7@gmail.com>
2023-05-10 20:56:36 +0100

    AboutMenu

--- Mobile_AR/Assets/Scripts/MobileDebug.cs
+++ Mobile_AR/Assets/Scripts/MobileDebug.cs
@@ -5,23 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class MobileDebug : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    public void OnPressButon()
-    {
-        Debug.Log("Button Pressed");
-    }
     public void LoadScene (string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
